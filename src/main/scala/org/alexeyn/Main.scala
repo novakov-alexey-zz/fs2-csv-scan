16183c8c2b95c1a1b164cad08f2e1372d2be397a
Alexey Novakov <novakov.alex@gmail.com>
2019-05-27 10:17:00 +0200

    use space char to merge broken lines

--- src/main/scala/org/alexeyn/Main.scala
+++ src/main/scala/org/alexeyn/Main.scala
@@ -13,7 +13,7 @@ import scala.util.matching.Regex
 
 object Main extends IOApp {
   val Separator = ";"
-  val isNextId: Regex = s"^(\\d+.*)$Separator".r
+  val isNextId: Regex = s"^(\\s*\\d+.*)$Separator".r
   val LastRowPaddingId = s"1$Separator"
   val ColumnsInFile = 10
   val HeaderLines = 1
@@ -50,10 +50,10 @@ object Main extends IOApp {
     // next line detected, i.e. we flush `acc` downstream since it already contains complete line to be processed
     if (isNextId.findFirstIn(acc).isDefined && isNextId.findFirstIn(line).isDefined) (line, acc)
     // next line is not yet detected, i.e. we flush empty string and append current line to the current `acc` state
-    else (acc.replace("\n", " ") + line, "")
+    else (acc + " " + line, "")
   }
 
   private def processRow(columns: Array[String]): Unit = {
-    println(s"processed: ${columns.mkString(" :: ")}")
+    println(s"processed: ${columns.mkString(" :: ").trim}")
   }
 }
